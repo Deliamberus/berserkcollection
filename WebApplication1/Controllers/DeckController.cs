f041271a8b11a0bf1f4df306f2b7303b5f555544
Andrey Nasonov <andrey.nasonov@informicus.ru>
2024-08-21 09:35:43 +0300

    Добавил сет ложных богов и заготовки под сбор деки

---
+++ WebApplication1/Controllers/DeckController.cs
@@ -0,0 +1,67 @@
+﻿using BerserkCollection.Domain.Entities;
+using BerserkCollection.Domain.Repositories;
+using BerserkCollection.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BerserkCollection.Controllers
+{
+    public class DeckController : Controller
+    {
+        private readonly CardRepository _cardRepository;
+        private readonly CollectionRepository _collectionRepository;
+
+        public DeckController(CardRepository cardRepository, CollectionRepository collectionRepository)
+        {
+            _cardRepository = cardRepository;
+            _collectionRepository = collectionRepository;
+        }
+
+        public IActionResult Edit()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var allCards = _cardRepository.GetAllCards();
+            var userCards = _collectionRepository.GetUserCards(userId);
+
+            return View(GetCollectionViewModels(allCards, userCards));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult SaveCard([FromBody] SaveCardViewModel card)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            _collectionRepository.SaveCard(userId, card);
+            return Ok("ok");
+        }
+
+        public List<CollectionViewModel> GetCollectionViewModels(List<Card> cards, List<UserCards> userCards)
+        {
+            List<CollectionViewModel> collection = new List<CollectionViewModel>();
+
+            foreach (Card card in cards)
+            {
+                CollectionViewModel collectionItem = new CollectionViewModel(card);
+                var userCard = userCards.FirstOrDefault(x => x.Card.Id == card.Id);
+                if (userCard != null)
+                {
+                    collectionItem.Count = userCard.Count;
+                }
+                collection.Add(collectionItem);
+            }
+
+            return collection;
+        }
+    }
+}
